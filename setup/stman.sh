dac75cc7810101c984a01fc4a10c87f137df3220
Charles Kerr <ckerr@github.com>
2018-04-12 10:44:28 -0500

    Tighter permissions on stman config files

--- setup/stman.sh
+++ setup/stman.sh
@@ -1,9 +1,11 @@
 #/usr/bin/env bash
 
-# create the config file that stman requires
-config_dir=${XDG_CONFIG_HOME:-$HOME/.config}
 pkg=syncthingmanager
-pkg_config_dir=${config_dir}/${pkg}
-rm ${pkg_config_dir}/${pkg}.conf
-rmdir ${pkg_config_dir}
+pkg_config_dir=${XDG_CONFIG_HOME:-$HOME/.config}/${pkg}
+
+# create the config file that stman requires
+rm ${pkg_config_dir}/${pkg}.conf 2>/dev/null
+rmdir ${pkg_config_dir} 2>/dev/null
 stman configure --port 8080
+chmod 770 ${pkg_config_dir}
+chmod 600 ${pkg_config_dir}/${pkg}.conf
