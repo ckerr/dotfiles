50069a57f870800dc53b7e501119f4aaefc664f5
Charles Kerr <ckerr@github.com>
2018-04-12 10:59:45 -0500

    Add initial syncthing pairing to stman

--- setup/stman.sh
+++ setup/stman.sh
@@ -1,11 +1,17 @@
 #/usr/bin/env bash
+. assets/env.sh
 
+# create the config file so that stman can run
+config_dir=${XDG_CONFIG_HOME:-$HOME/.config}
 pkg=syncthingmanager
-pkg_config_dir=${XDG_CONFIG_HOME:-$HOME/.config}/${pkg}
-
-# create the config file that stman requires
-rm ${pkg_config_dir}/${pkg}.conf 2>/dev/null
-rmdir ${pkg_config_dir} 2>/dev/null
+pkg_config_dir=${config_dir}/${pkg}
+rm ${pkg_config_dir}/${pkg}.conf
+rmdir ${pkg_config_dir}
 stman configure --port 8080
-chmod 770 ${pkg_config_dir}
-chmod 600 ${pkg_config_dir}/${pkg}.conf
+
+# connect to the mothership
+stman device add \
+  --name "${STMAN_ADD_DEVICE_NAME}" \
+  --address "${STMAN_ADD_DEVICE_ADDRESS}" \
+  "${STMAN_ADD_DEVICE_ID}"
+echo "please check ${STMAN_ADD_DEVICE_NAME} for a pair request"
