501727ce29c059de32900d452ec1236391a61521
Charles Kerr <ckerr@github.com>
2018-04-11 19:35:38 -0500

    Better structured macOS brew installs

--- brew.sh
+++ brew.sh
@@ -1,51 +1,110 @@
 #!/usr/bin/env bash
 
-BREW_APPS=( coreutils syncthing mpv vim macvim xz )
+BREW_APPS=(
+  coreutils
+  macvim
+  mpv
+  ripgrep
+  vim
+  xz
+)
 
-CASK_APPS=( atom font-awesome-terminal-fonts keepassxc meld vagrant vagrant-manager virtualbox virtualbox-extension-pack )
+BREW_SERVICES=(
+  syncthing
+)
 
+CASK_APPS=(
+  atom
+  beyond-compare
+  font-awesome-terminal-fonts
+  keepassxc
+  meld
+  vagrant
+  vagrant-manager
+  virtualbox
+  virtualbox-extension-pack
+)
 
-if [ "Darwin" == "$(uname -s)" ]; then
+##
+##
 
-  # install brew if we don't have it already
-  item="brew"
-  if [ "" == $(command -v ${item}) ]; then
-    echo "installing $item"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    if [[ $? != 0 ]]; then
-      error "error installing $item -> quitting setup"
-      exit 2
-    fi
-  fi
-
-  # update brew
-  echo "updating brew"
-  brew prune
-  brew doctor
-  brew update
-
-  # install brew apps
-  for app in "${BREW_APPS[@]}"
-  do
-    brew install $app
-    if [[ $? != 0 ]]; then
-      error "failed to install $app! aborting..."
-      exit -1
-    fi
-  done
-  brew services start syncthing
-
-  # install cask
-  brew tap caskroom/cask
-
-  # install apps
-  for app in "${CASK_APPS[@]}"
-  do
-    brew cask install $app
-    if [[ $? != 0 ]]; then
-      error "failed to install $app! aborting..."
-      exit -1
-    fi
-  done
-
+# we only need this file on mac
+if [ "Darwin" != "$(uname -s)" ]; then
+  exit 0
 fi
+
+##
+##
+
+function exit_if_error()
+{
+  if [[ $? != 0 ]]; then
+    echo "$1 failed! aborting..."
+    exit 1
+  fi
+}
+
+function brew_install()
+{
+  item=$1
+
+  count="$(brew list -1 | grep --count $item)"
+  if [ "0" -eq "${count}" ]; then
+    brew install "${item}"
+    exit_if_error "${item}"
+  else
+    echo "already installed: ${item}"
+  fi
+}
+
+function cask_install()
+{
+  item=$1
+
+  count="$(brew cask list -1 | grep --count $item)"
+  if [ "0" -eq "${count}" ]; then
+    brew cask install "${item}"
+    exit_if_error "${item}"
+  else
+    echo "already installed: ${item}"
+  fi
+}
+
+##
+##
+
+# ensure brew is installed
+item="brew"
+if [ "" == $(command -v ${item}) ]; then
+  echo "installing $item"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  exit_if_error $item
+fi
+
+# update brew
+echo "updating brew"
+brew prune
+brew doctor
+brew update
+
+# install brew apps
+for item in "${BREW_APPS[@]}"
+do
+  brew_install $item
+done
+
+# install brew services
+for item in "${BREW_SERVICES[@]}"
+do
+  brew_install $item
+  brew services start $item
+done
+
+# install cask
+brew tap caskroom/cask
+
+# install cask apps
+for item in "${CASK_APPS[@]}"
+do
+  cask_install $item
+done
