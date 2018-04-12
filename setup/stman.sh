52d5d029495daaa7af9df07604efab3bb1ab49aa
Charles Kerr <ckerr@github.com>
2018-04-12 16:02:01 -0500

    Set local SyncThing instance's name

--- setup/stman.sh
+++ setup/stman.sh
@@ -1,4 +1,5 @@
 #/usr/bin/env bash
+
 . assets/env.sh
 
 # create the config file so that stman can run
@@ -7,11 +8,18 @@ pkg=syncthingmanager
 pkg_config_dir=${config_dir}/${pkg}
 rm ${pkg_config_dir}/${pkg}.conf
 rmdir ${pkg_config_dir}
-stman configure --port 8080
+stman configure
 
-# connect to the mothership
+# configure the local system
+my_id=$(stman device list | grep "ID:" | head -n1 | cut -d: -f2 | tr -d "[:blank:]")
+echo "local syncthing id is $my_id"
+echo "please enter a name for this syncthing device:"
+read name
+stman device edit --name "${name}" -io "${my_id}"
+
+# connect it to the mothership
 stman device add \
   --name "${STMAN_ADD_DEVICE_NAME}" \
   --address "${STMAN_ADD_DEVICE_ADDRESS}" \
   "${STMAN_ADD_DEVICE_ID}"
-echo "please check ${STMAN_ADD_DEVICE_NAME} for a pair request"
+echo "Please check ${STMAN_ADD_DEVICE_NAME} for a pair request, and share Passwords + SyncTemp"
