9d6b8ec04ea16794d484fa7e26c03495e2f85cdd
Charles Kerr <ckerr@github.com>
2018-04-12 11:21:37 -0500

    Fix tyop

--- decrypt.sh
+++ decrypt.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
 
-openssl enc -aes-256-cbc -d -a -in assets.tar.aes256 --md sha2565 | tar --extract
+openssl enc -aes-256-cbc -d -a -in assets.tar.aes256 -md sha256 | tar --extract
