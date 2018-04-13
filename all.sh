3e13e6445acf5dfb08ad9058931fe8095dee9407
Charles Kerr <ckerr@github.com>
2018-04-13 07:02:07 -0500

    Move all the scripts into a single directory

--- all.sh
+++ all.sh
@@ -1,14 +1,14 @@
 #/usr/bin/env bash
 
-install/apt.sh
-install/snap.sh
-install/brew.sh
-install/pip3.sh
+./install-apt.sh
+./install-snap.sh
+./install-brew.sh
+./install-pip3.sh
 
 # need to install oh-my-zsh for nvm before installing npm packages
 ./decrypt.sh
-install/zsh.sh
-install/npm.sh
+./install-omz.sh
+./install-npm.sh
 
-setup/stman.sh
-setup/pianobar.sh
+./setup-stman.sh
+./setup-pianobar.sh
