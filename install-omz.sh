cce8bb7b07b01dc3ddebfa7983b4c5b0106a9515
Charles Kerr <ckerr@github.com>
2018-04-16 10:00:11 -0500

    Move some omz helper functions into a shared file

--- install-omz.sh
+++ install-omz.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+. ./common.sh
+
 if [ "" != "$(command -v gsed)" ]; then
   sedcmd=gsed
 else
@@ -27,53 +29,6 @@ zshrc="${ZDOTDIR:-$HOME}/.zshrc"
 ###
 ###  Helpers
 ###
-
-# modify variables in a script file
-# looks for lines beginning with "KEY=" or "# KEY="
-# and inserts there
-function set_variable_in_file {
-  filename=$1
-  key=$2
-  val=$3
-
-  # if there's a line starting with "KEY=", replace it
-  if [ "$(grep --count "^$key=" "$filename")" -eq "1" ]; then
-    ${sedcmd} --in-place='' "s/^${key}=.*/${key}=\"${val}\"/" "${filename}"
-    return
-  fi
-
-  # if there's a line starting with "# KEY=", add our keyval there
-  if [ "$(grep --count "^# $key=" "$filename")" -eq "1" ]; then
-    ${sedcmd} --in-place='' "/^# ${key}=.*/a \\${key}=\"${val}\"" "${filename}"
-    return
-  fi
-
-  echo not found in $filename: $key
-}
-
-function get_repo {
-  name=$1
-  repo_url=$2
-  destination=$3
-
-  if [ -d "${destination}" ]; then
-    echo "updating ${name}"
-    pushd "${destination}"
-    git pull
-    popd
-  else
-    # ensure the parent directory exists
-    parent=$(dirname "${destination}")
-    if [ ! -d "${parent}" ]; then
-      mkdir -p "${parent}"
-      chmod 750 "${parent}"
-    fi
-
-    echo "installing ${name}"
-    env git clone -q --depth=1 "${repo_url}" "${destination}"
-    chmod 750 "${destination}"
-  fi
-}
 
 function add_to_omz_plugin_list {
   name=$1
