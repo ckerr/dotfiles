156a871b23e83dfd5d20668075f9f87019886ff3
Charles Kerr <ckerr@github.com>
2018-08-12 09:52:24 -0700

    remove electron-centric commands from git.zsh

--- assets/public/zsh-custom/git.zsh
+++ assets/public/zsh-custom/git.zsh
@@ -7,18 +7,3 @@ function gpob {
   branch_name=`git rev-parse --abbrev-ref HEAD`
   git push --verbose origin "${branch_name}"
 }
-
-##
-##
-
-#function electron_cd {
-#  #echo "$1"
-#  cd "/home/charles/src/electron/electron/$1"
-#  return 0
-#}
-#
-#alias ecd18='electron_cd "1-8-x"'
-#alias ecd17='electron_cd "1-7-x"'
-#alias ecd2='electron_cd "2-0-x"'
-#alias ecd3='electron_cd "3-0-x"'
-#alias ecdm='electron_cd "master"'
