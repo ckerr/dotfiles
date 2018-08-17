8d5cf70d200325fd020c99569ecb4cac39428205
Charles Kerr <ckerr@github.com>
2018-08-17 05:47:36 -0700

    fix: silence dir-exists warning
    check to see if GIT_CACHE_PATH exists before trying to mkdir it

--- assets/public/zsh-custom/electron.env.zsh
+++ assets/public/zsh-custom/electron.env.zsh
@@ -2,4 +2,6 @@ export PATH="${PATH}:${HOME}/src/depot_tools"
 
 # used by depot tools
 export GIT_CACHE_PATH="${HOME}/.git_cache"
-mkdir "${GIT_CACHE_PATH}"
+if [ ! -d "${GIT_CACHE_PATH}" ]; then
+  mkdir "${GIT_CACHE_PATH}"
+fi
