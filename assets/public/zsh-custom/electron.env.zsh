6f98b224c4c61321150c3ce14916ff31bd43d0e7
Charles Kerr <ckerr@github.com>
2018-08-17 14:17:12 -0700

    feat: use electron's sccache

--- assets/public/zsh-custom/electron.env.zsh
+++ assets/public/zsh-custom/electron.env.zsh
@@ -5,3 +5,7 @@ export GIT_CACHE_PATH="${HOME}/.git_cache"
 if [ ! -d "${GIT_CACHE_PATH}" ]; then
   mkdir "${GIT_CACHE_PATH}"
 fi
+
+# used by electron's sccache to share cache data with CI
+export SCCACHE_BUCKET="electronjs-sccache"
+export SCCACHE_TWO_TIER=true
