37a59ac97557c0686d19cb9e4a1c7cd2f5ad3665
Charles Kerr <ckerr@github.com>
2018-08-18 15:08:56 -0700

    fix: copy-paste error

--- assets/public/zsh-custom/electron.env.zsh
+++ assets/public/zsh-custom/electron.env.zsh
@@ -7,7 +7,7 @@ mkdir -p "${GIT_CACHE_PATH}"
 
 # used by sccache
 export SCCACHE_DIR="${HOME}/.electron-cache/sccache"
-mkdir -p "${GIT_CACHE_PATH}"
+mkdir -p "${SCCACHE_DIR}"
 
 # used by electron's branch of sccache to share with CI
 export SCCACHE_BUCKET="electronjs-sccache"
