01e1b411724e807a4dfb516fa9fc4d30637ba12f
Charles Kerr <ckerr@github.com>
2018-08-25 10:00:12 -0500

    fix: script typo

--- assets/public/dotfiles/.zshenvd/misc.zsh
+++ assets/public/dotfiles/.zshenvd/misc.zsh
@@ -3,6 +3,6 @@ export PAGER=less
 export TMPDIR=/tmp
 
 # on Ubuntu, add Snappy to our path
-if [ "Ubuntu" == "$(lsb_release --id --short)" ]; then
+if [ "Ubuntu" = "$(lsb_release --id --short)" ]; then
   export PATH="$PATH:/snap/bin"
 fi
