4c43fb7fe047bd180a0265c7e2197cd48fbbb703
Charles Kerr <ckerr@github.com>
2018-08-25 16:10:59 -0500

    fix: bash compatibility in zshenv files

--- assets/public/dotfiles/.zshenvd/misc.zsh
+++ assets/public/dotfiles/.zshenvd/misc.zsh
@@ -12,17 +12,18 @@ export QT_AUTO_SCREEN_SCALE_FACTOR=1
 # PATH building
 
 ## Snappy
+
 if [ "Ubuntu" = "$(lsb_release --id --short)" ]; then
   export PATH="$PATH:/snap/bin"
 fi
 
 ## Personal bindirs
-personal_bindirs = (
-  $HOME/opt/bin
-  $HOME/.local/bin
-  $HOME/bin
-)
 
+personal_bindirs=(
+  "${HOME}"/opt/bin
+  "${HOME}"/.local/bin
+  "${HOME}"/bin
+)
 for bindir in "${personal_bindirs[@]}"
 do
   if [ -d "${bindir}" ]; then
