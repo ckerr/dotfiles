79b8b5d96778bf59366299eb2df369cf77ddf84d
Charles Kerr <ckerr@github.com>
2018-08-25 14:16:53 -0500

    feat: add QT_AUTO_SCREEN_SCALE_FACTOR to env

--- assets/public/dotfiles/.zshenvd/misc.zsh
+++ assets/public/dotfiles/.zshenvd/misc.zsh
@@ -6,3 +6,7 @@ export TMPDIR=/tmp
 if [ "Ubuntu" = "$(lsb_release --id --short)" ]; then
   export PATH="$PATH:/snap/bin"
 fi
+
+# https://doc.qt.io/qt-5/highdpi.html
+# enables automatic scaling, based on the pixel density of the monitor.
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
