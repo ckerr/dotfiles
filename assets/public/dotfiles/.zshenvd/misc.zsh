6115214734792f7f300ca105a29f142d84eb5e7d
Charles Kerr <ckerr@github.com>
2018-08-25 16:21:13 -0500

    refactor: set XDG_CACHE_DIR in zshenv

--- assets/public/dotfiles/.zshenvd/misc.zsh
+++ assets/public/dotfiles/.zshenvd/misc.zsh
@@ -9,6 +9,15 @@ export TMPDIR=/tmp
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 
 
+# Cache directory
+
+XDG_CACHE_HOME="${TMPDIR}/charles-cache"
+if [ ! -d ${XDG_CACHE_HOME} ]; then
+  mkdir --mode=750 "${XDG_CACHE_HOME}"
+fi
+export XDG_CACHE_HOME
+
+
 # PATH building
 
 ## Snappy
