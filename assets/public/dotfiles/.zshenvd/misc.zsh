5666358349554b7a4be7fe5bf571c0b2655fd512
Charles Kerr <ckerr@github.com>
2018-08-25 16:04:35 -0500

    fix: add local bindirs to PATH in zshenv

--- assets/public/dotfiles/.zshenvd/misc.zsh
+++ assets/public/dotfiles/.zshenvd/misc.zsh
@@ -1,12 +1,31 @@
-export EDITOR=vim
+# Misc settings
+
+export EDITOR=/usr/bin/vim
 export PAGER=less
 export TMPDIR=/tmp
-
-# on Ubuntu, add Snappy to our path
-if [ "Ubuntu" = "$(lsb_release --id --short)" ]; then
-  export PATH="$PATH:/snap/bin"
-fi
 
 # https://doc.qt.io/qt-5/highdpi.html
 # enables automatic scaling, based on the pixel density of the monitor.
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
+
+
+# PATH building
+
+## Snappy
+if [ "Ubuntu" = "$(lsb_release --id --short)" ]; then
+  export PATH="$PATH:/snap/bin"
+fi
+
+## Personal bindirs
+personal_bindirs = (
+  $HOME/opt/bin
+  $HOME/.local/bin
+  $HOME/bin
+)
+
+for bindir in "${personal_bindirs[@]}"
+do
+  if [ -d "${bindir}" ]; then
+    export PATH="${bindir}:${PATH}"
+  fi
+done
