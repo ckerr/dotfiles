f6ef759e9cdd860b687c2a7e80d0d46d213b1aa0
Charles Kerr <ckerr@github.com>
2018-08-27 16:00:08 -0500

    chore: use sh in encrypt, decrypt shebang

--- decrypt.sh
+++ decrypt.sh
@@ -1,3 +1,3 @@
-#/usr/bin/env bash
+#/usr/bin/env sh
 
 openssl enc -aes-256-cbc -d -a -in assets-private.tar.aes256 -md sha256 | tar --extract
--- encrypt.sh
+++ encrypt.sh
@@ -1,3 +1,3 @@
-#/usr/bin/env bash
+#/usr/bin/env sh
 
 tar --create --to-stdout assets/private/ | openssl enc -aes-256-cbc -e -a -out assets-private.tar.aes256
