cf50b3b5b8ae762fd5a1925a07dfa0345e30a237
Charles Kerr <ckerr@github.com>
2018-10-01 09:59:50 -0500

    chore: disable QT_AUTO_SCREEN_SCALE_FACTOR env
    it's no longer necessary on my Dell

--- assets/public/dotfiles/.profile.d/misc.sh
+++ assets/public/dotfiles/.profile.d/misc.sh
@@ -6,7 +6,7 @@ export TMPDIR=/tmp
 
 # https://doc.qt.io/qt-5/highdpi.html
 # enables automatic scaling, based on the pixel density of the monitor.
-export QT_AUTO_SCREEN_SCALE_FACTOR=1
+# export QT_AUTO_SCREEN_SCALE_FACTOR=1
 
 
 # Cache directory
