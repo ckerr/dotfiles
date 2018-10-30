b1d5f1d9c04e967a9fc0e00686d3a18c778e14b5
Charles Kerr <ckerr@github.com>
2018-10-30 12:10:59 -0500

    feat: add TERM=xterm-256color

--- assets/public/dotfiles/.profile.d/zsh.sh
+++ assets/public/dotfiles/.profile.d/zsh.sh
@@ -2,3 +2,9 @@
 export HISTFILE=~/.zsh_history
 export HISTSIZE=10000
 export SAVEHIST=${HISTSIZE}
+
+# WARNING! Your terminal appears to support fewer than 256 colors!
+# If your terminal supports 256 colors, please export the appropriate environment variable
+# _before_ loading this theme in your ~/.zshrc. In most terminal emulators, putting
+# export TERM="xterm-256color" at the top of your ~/.zshrc is sufficient.
+export TERM="xterm-256color"
