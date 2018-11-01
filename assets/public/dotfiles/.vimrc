0f8e78140e9296803bcde5203dcb84bd1300d0fb
Charles Kerr <ckerr@github.com>
2018-11-01 15:53:08 -0500

    chore: farm .vimrc out to the dotfiles dir

---
+++ assets/public/dotfiles/.vimrc
@@ -0,0 +1,5 @@
+:set shiftwidth=2
+:set expandtab
+:syntax on
+:colorscheme afterglow
+:let g:javascript_plugin_jsdoc=1
--- setup-vim.sh
+++ setup-vim.sh
@@ -2,12 +2,7 @@
 
 . ./common.sh
 
-COLORSCHEME=afterglow
-viminit="${VIMINIT:-${HOME}/.vimrc}"
 vimplugindir="${VIMRUNTIME:-${HOME}/.vim}/pack/plugins/start"
-
-
-# Update plugins
 
 mkdir -vp "${vimplugindir}"
 
@@ -20,11 +15,4 @@ get_repo "${name}" "https://github.com/octol/${name}.git" "${vimplugindir}/${nam
 name="vim-javascript"
 get_repo "${name}" "https://github.com/pangloss/${name}.git" "${vimplugindir}/${name}"
 
-
-# Initialize .vimrc
-
-touch "${viminit}"
-set_variable_in_file "${viminit}" ":set\ shiftwidth=" ":set shiftwidth=2"
-set_variable_in_file "${viminit}" ":set\ expandtab" ":set expandtab"
-set_variable_in_file "${viminit}" ":syntax" ":syntax on"
-set_variable_in_file "${viminit}" ":colorscheme\ " ":colorscheme ${COLORSCHEME}"
+echo $0 done
