fa860baa185d5dbfc593261d9f68ab8726cfa594
Charles Kerr <ckerr@github.com>
2019-02-11 13:29:48 -0600

    add -pbkdf2 to encrypt/decrypt steps

--- decrypt.sh
+++ decrypt.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env sh
 
-openssl enc -aes-256-cbc -d -a -in assets-private.tar.aes256 -md sha256 | tar --extract
+openssl enc -aes-256-cbc -d -a -pbkdf2 -in assets-private.tar.aes256 -md sha256 | tar --extract
--- encrypt.sh
+++ encrypt.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env sh
 
-tar --create --to-stdout assets/private/ | openssl enc -aes-256-cbc -e -a -out assets-private.tar.aes256
+tar --create --to-stdout assets/private/ | openssl enc -aes-256-cbc -e -a -pbkdf2 -out assets-private.tar.aes256
