c332a685bcba34b895760aa7075e34a444f48054
Charles Kerr <ckerr@github.com>
2019-06-03 11:43:00 -0500

    create scripts directory for pngmin

--- install-files.zsh
+++ install-files.zsh
@@ -17,4 +17,8 @@ fi
   && $gfind -name ".[^.]*"    -type f -exec $ginstall -Dm 600 "{}" "${HOME}/{}" \; -print \
   && $gfind -path ".[^.]*/**" -type f -exec $ginstall -Dm 600 "{}" "${HOME}/{}" \; -print )
 
+(cd assets/public/scripts \
+  && $gfind -name ".[^.]*"    -type f -exec $ginstall "{}" "${HOME}/{}" \; -print \
+  && $gfind -path ".[^.]*/**" -type f -exec $ginstall "{}" "${HOME}/{}" \; -print )
+
 echo $0 done
