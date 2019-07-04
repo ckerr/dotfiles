cf3c79f71b6a618b84b88d20cc25621f4dcba30b
Charles Kerr <ckerr@github.com>
2019-07-04 18:36:07 -0500

    feat: add vim typescript syntax highlighting

--- setup-vim.sh
+++ setup-vim.sh
@@ -6,17 +6,25 @@ vimplugindir="${vimruntime}/pack/plugins/start"
 
 mkdir -vp "${vimplugindir}"
 
-name="awesome-vim-colorschemes"
-get_repo "${name}" "https://github.com/rafi/${name}.git" "${vimplugindir}/${name}"
+user='rafi'
+name='awesome-vim-colorschemes'
+get_repo "${name}" "https://github.com/${user}/${name}.git" "${vimplugindir}/${name}"
 
-name="vim-cpp-enhanced-highlight"
-get_repo "${name}" "https://github.com/octol/${name}.git" "${vimplugindir}/${name}"
+user='octol'
+name='vim-cpp-enhanced-highlight'
+get_repo "${name}" "https://github.com/${user}/${name}.git" "${vimplugindir}/${name}"
 
-name="vim-javascript"
-get_repo "${name}" "https://github.com/pangloss/${name}.git" "${vimplugindir}/${name}"
+user='pangloss'
+name='vim-javascript'
+get_repo "${name}" "https://github.com/${user}/${name}.git" "${vimplugindir}/${name}"
 
-name="YouCompleteMe"
-get_repo "${name}" "https://github.com/Valloric/${name}.git" "${vimplugindir}/${name}"
+user='leafgarland'
+name='typescript-vim'
+get_repo "${name}" "https://github.com/${user}/${name}.git" "${vimplugindir}/${name}"
+
+user='Valloric'
+name='YouCompleteMe'
+get_repo "${name}" "https://github.com/${user}/${name}.git" "${vimplugindir}/${name}"
 pushd "${vimplugindir}/${name}"
 git submodule update --init --recursive
 ./install.py
