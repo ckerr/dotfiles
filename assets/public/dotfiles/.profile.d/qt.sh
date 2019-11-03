b48d86a617f776bf68c175eef520ac7ffd6f5bc5
Charles Kerr <ckerr@github.com>
2019-11-03 16:50:54 -0600

    feat: add QT environment variables

---
+++ assets/public/dotfiles/.profile.d/qt.sh
@@ -0,0 +1,7 @@
+# suppress “qt5ct: using qt5ct plugin” message
+export QT_LOGGING_RULES="qt5ct.debug=false" 
+
+# This was needed on disco but seems to not be needed on eoan?
+# export QT_AUTO_SCREEN_SCALE_FACTOR=0
+# export QT_SCREEN_SCALE_FACTORS='1;1'
+
