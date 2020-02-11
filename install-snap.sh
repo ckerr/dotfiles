eaad0b0d69e2004fa489a89b98c54fc1f31a2407
Charles Kerr <charles@charleskerr.com>
2020-02-10 21:38:07 -0600

    feat: add foliate snap

--- install-snap.sh
+++ install-snap.sh
@@ -1,3 +1,4 @@
 sudo snap refresh
-sudo snap install --classic --stable ripgrep
+sudo snap install           --stable foliate
 sudo snap install           --stable telegram-desktop
+sudo snap install --classic --stable ripgrep
