e37ed004d9f4ca4280fb6ace16dd934b4982c439
Charles Kerr <ckerr@github.com>
2020-04-20 18:38:53 -0500

    feat: add ~/.lessfilter to display markdown files

--- assets/public/dotfiles/.profile.d/less.sh
+++ assets/public/dotfiles/.profile.d/less.sh
@@ -1,4 +1,3 @@
-# -R : Like -r, but only ANSI "color" escape sequences are output in "raw" form. Unlike -r, the screen appearance is
-# -F or --quit-if-one-screen : Causes less to automatically exit if the entire file can be displayed on the first screen.
-# -X or --no-init : Disables sending the termcap initialization and deinitialization strings to the terminal.
-export LESS="-RFX"
+# see lesspipe manpage.
+# this is a prerequisite for ~/.lessfilter
+eval "$(lesspipe)"
