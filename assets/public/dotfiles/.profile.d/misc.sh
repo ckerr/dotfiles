962d9600ee2d313c4e1ab40d9ffb2d361653586f
Charles Kerr <charles@charleskerr.com>
2026-07-30 11:50:56 -0500

    refactor: do not put XDG_CACHE_HOME in ${TMPDIR}

--- assets/public/dotfiles/.profile.d/misc.sh
+++ assets/public/dotfiles/.profile.d/misc.sh
@@ -10,15 +10,6 @@ export TMPDIR=/tmp
 # export QT_AUTO_SCREEN_SCALE_FACTOR=1
 
 
-# Cache directory
-
-XDG_CACHE_HOME="${TMPDIR}/charles-cache"
-if [ ! -d ${XDG_CACHE_HOME} ]; then
-  mkdir -m 700 "${XDG_CACHE_HOME}"
-fi
-export XDG_CACHE_HOME
-
-
 # PATH building
 
 bindirs=(
